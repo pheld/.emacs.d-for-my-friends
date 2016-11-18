37de7f17d2da8ad3ac97acf580f434cf87cec6c2
pheld <peter.t.held@gmail.com>
2016-11-17 23:39:44 -0800

    Fix startup errors

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (let ((default-directory "~/.emacs.d/"))
   (normal-top-level-add-subdirs-to-load-path))
 
@@ -24,7 +31,6 @@
    itail
    powerline
    smart-indent-rigidly
-   alchemist
    my-theme
    my-isearch
    my-advice
