18341d732c16e289dd9033721c888e4010f3b0e3
Peter Held <pheld@flipboard.com>
2019-05-13 20:46:54 -0700

    Use stable Melpa

--- my/my-package.el
+++ my/my-package.el
@@ -1,5 +1,5 @@
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (provide 'my-package)
